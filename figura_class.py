566e979601a605986d4d29fbab83b73eb2092e65
ivanes79 <ivanes666@hotmail.es>
2022-12-07 18:33:02 +0100

    terminar juego por tiempo y marcador, clase menu y logica de menu a partida

--- figura_class.py
+++ figura_class.py
@@ -1,5 +1,94 @@
 import pygame as pg
 
+class Pelota:
+    def __init__(self,pos_x,pos_y,radio=20,color=(255,255,255),vx=1,vy=1):
+        self.pos_x = pos_x
+        self.pos_y =pos_y 
+        self.radio = radio
+        self.color = color
+        self.vx = vx
+        self.vy = vy
+        self.font = pg.font.Font(None, 40)
+
+
+    def dibujar(self,pantalla):
+        pg.draw.circle(pantalla,self.color,(self.pos_x,self.pos_y),self.radio)
+
+    def mover(self,y_max=600,x_max=800):
+        self.pos_x += self.vx
+        self.pos_y += self.vy
+
+        if self.pos_y >= y_max-self.radio or self.pos_y < 0+self.radio:#para que rebote arriba y abajo
+            self.vy *= -1 
+            #objetivo que la pelota desaparezca en los limites
+            # y vuelva a aparecer rebotando hacia el lado contrario
+            #desde donde vino
+            #contar el gol nuevo desafio
+        if self.pos_x >= x_max+self.radio*10:#limite derecho
+            self.pos_x = x_max//2
+            self.pos_y = y_max//2
+            self.vx *= -1
+            self.vy *= -1
+
+            return "right"
+             
+
+        if self.pos_x < 0-self.radio*10:#limite izquierdo
+            self.pos_x = x_max//2
+            self.pos_y = y_max//2
+            self.vx *= -1
+            self.vy *= -1
+
+            return "left"
+    
+    
+
+    @property
+    def derecha(self):
+        return self.pos_x + self.radio
+    @property
+    def izquierda(self):
+        return self.pos_x - self.radio
+    @property
+    def arriba(self):
+        return self.pos_y - self.radio
+    @property
+    def abajo(self):
+        return self.pos_y + self.radio      
+              
+
+    def comprobar_choque(self,r1,r2):
+        if self.derecha  >= r2.izquierda and \
+            self.izquierda  <= r2.derecha and \
+            self.abajo >= r2.arriba and\
+            self.arriba <= r2.abajo:
+            self.vx *= -1
+
+        if self.derecha  >= r1.izquierda and \
+            self.izquierda  <= r1.derecha and \
+            self.abajo >= r1.arriba and\
+            self.arriba <= r1.abajo:
+            self.vx *= -1
+
+    def comprobar_choqueV2(self,*raquetas):
+        for r in raquetas:
+            if self.derecha  >= r.izquierda and \
+               self.izquierda  <= r.derecha and \
+               self.abajo >= r.arriba and\
+               self.arriba <= r.abajo:
+                    self.vx *= -1
+                    break
+   
+
+
+              
+    
+
+            
+
+
+
+
 class Raqueta:
     def __init__(self,pos_x,pos_y,w=20,h=100,color=(255,255,255),vx=1,vy=1):
         self.pos_x = pos_x
@@ -12,165 +101,30 @@ class Raqueta:
 
     def dibujar(self,pantalla):
         pg.draw.rect(pantalla,self.color,(self.pos_x-(self.w//2),self.pos_y-(self.h//2),self.w,self.h))    
-    '''
-    def mover(self,tecla_arriba,tecla_abajo,ymax =600,ymin=0):
-        estado_teclas = pg.key.get_pressed()
         
-        if estado_teclas[tecla_arriba] == True and self.pos_y > (ymax-self.h//2):
-            self.pos_y -= 1
-        if estado_teclas[tecla_abajo] == True and self.pos_y > (ymin-self.h//2):
-            self.pos_y += 1 
-    '''
+
     def mover(self,tecla_arriba,tecla_abajo,y_max=600,y_min=0):
         estado_teclas = pg.key.get_pressed()
        
         if estado_teclas[tecla_arriba] == True and self.pos_y > (y_min+self.h//2):
             self.pos_y -= 1
         if estado_teclas[tecla_abajo] == True and self.pos_y < (y_max-self.h//2) :
-            self.pos_y += 1 
+            self.pos_y += 1     
 
-
-    @property # para poder tratar estas funciones como variables , sin ()
-    def derecha(self):
-        return self.pos_x + self.w//2
-    @property
-    def izquierda(self):
-        return self.pos_x - self.w//2
     @property
     def arriba(self):
         return self.pos_y - self.h//2
+
     @property
     def abajo(self):
-        return self.pos_y + self.h//2    
-
-class Pelota:
-    def __init__(self,pos_x,pos_y,radio=20,color=(255,255,255),vx=1,vy=1):
-        self.pos_x = pos_x
-        self.pos_y =pos_y 
-        self.radio = radio
-        self.color = color
-        self.vx = vx
-        self.vy = vy
-        self.contadorderecha = 0
-        self.contadorizquierda = 0
-        self.font= pg.font.Font(None,40)
+        return self.pos_y + self.h//2
     
-    def Mover(self,xmax,ymax):
-        
-        self.pos_x += self.vx
-        self.pos_y += self.vy     
-
-
-        if  self.pos_y >= ymax-self.radio or self.pos_y <= 0 + self.radio :
-            self.vy *= -1
-        #objetivo que l apelota desaparezca en los limites y aparezca rebotando hacia el lado contrario
-
-        if self.pos_x >= xmax+self.radio*10: #limite derecha
-            self.contadorizquierda += 1
-
-            self.pos_x = xmax//2
-            self.pos_y = ymax//2
-
-            self.vx *= -1
-            self.vy *= -1
-            
-
-        if self.pos_x <= 0 - self.radio*10 : #limite izquierda
-            self.contadorderecha += 1
-
-            self.pos_x = xmax//2
-            self.pos_y = ymax//2            
-
-            self.vx *= -1
-            self.vy *= -1
-
-    def marcador(self,pantalla_principal):
-        #font= pg.font.Font(None,40) fuera del while
-        # dentro del while
-        marcador_derecha = self.font.render(str(self.contadorizquierda),0,(255,255,0))
-        marcador_izquierda = self.font.render(str(self.contadorderecha),0,(255,255,0))
-        pantalla_principal.blit(marcador_derecha,(200,50))
-        pantalla_principal.blit(marcador_izquierda,(600,50))
-
-
-    @property # para poder tratar estas funciones como variables , sin ()
-    def derecha(self):
-        return self.pos_x + self.radio
     @property
     def izquierda(self):
-        return self.pos_x - self.radio
-    @property
-    def arriba(self):
-        return self.pos_y - self.radio
-    @property
-    def abajo(self):
-        return self.pos_y + self.radio
-
-    def comprobar_choque(self,r1,r2):
-        if self.derecha >= r2.izquierda and \
-            self.izquierda <= r2.derecha and \
-            self.abajo >= r2.arriba and \
-            self.arriba <= r2.abajo:
-            self.vx *= -1
-
-
-        if self.derecha >= r1.izquierda and \
-            self.izquierda <= r1.derecha and \
-            self.abajo >= r1.arriba and \
-            self.arriba <= r1.abajo:
-            self.vx *= -1
-    def comprobar_choque2(self,*raquetas):
-        for r in raquetas:
-
-            if self.derecha >= r.izquierda and \
-                self.izquierda <= r.derecha and \
-                self.abajo >= r.arriba and \
-                self.arriba <= r.abajo:
-                    self.vx *= -1
-                    break #o return
-
-
-
-    '''
-    def posicionX(self):
-
-        return self.pos_x + self.radio 
+        return self.pos_x - self.w//2
     
-    def posicionY(self):
-        return self.pos_y + self.radio 
-
-    def izquierda(self):       
-        if self.pos_x < 400 :
-            return True
-        return False
-
-    def derecha(self): 
-        if self.pos_x > 400 :
-            return True
-        return False 
-
-    def arriba(self):
-        if self.pos_y < 300 :
-            return True
-        return False 
-
-
-    def abajo(self):
-        if self.pos_y > 300 :
-            return True
-        return False  
-    '''
-
-    '''
-        if  self.pos_x >= xmax-self.radio or self.pos_x <= 0 + self.radio :
-            self.vx *= -1
-            
-        if  self.pos_y >= ymax-self.radio or self.pos_y <= 0 + self.radio :
-            self.vy *= -1
-    '''
-
-
-    def dibujar(self,pantalla):
-        pg.draw.circle(pantalla,self.color,(self.pos_x,self.pos_y),self.radio)
+    @property
+    def derecha(self):
+        return self.pos_x + self.w//2    
 
   
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 
 import pygame as pg
-from pantallas import Partida
+from pantallas import Partida,Menu
 
 
 juego = Partida()#creamos el objeto de clase partida
 
 juego.bucle_fotograma()# llamamos al bucle de fotograma
+
+
+menu = Menu()
+mensaje = menu.bucle_pantalla()
+
+if mensaje == 'jugar':
+    juego = Partida() # creamos objeto clase partida
+    juego.bucle_fotograma() #llamamos al bucle fotograma
+
+#crear otra ventana para decir quien es el ganador
+
